79856072068d26f92280446afddd5d25ffaa8741
Aleksandr Kross <krossale@fel.cvut.cz>
2025-11-09 02:06:32 +0100

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,14 +19,10 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar__logo">
-        <img src={logo} alt="logo" className="logo-img" />
-          <Link to="/" className="navbar__logo">
+      <Link to="/" className="navbar__logo">
+        <img src={logo} alt="TodoList logo" className="logo-img" />
+        <span>TodoList</span>
       </Link>
-
-
-        <Link to="/">TodoList</Link>
-      </div>
 
       <div className="navbar__links">
         {user ? (
